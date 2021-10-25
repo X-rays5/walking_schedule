45cb6bce2bee33b0f90449127558d84dfe984be6
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-10-25 18:17:28 +0200

    implemented view walk

--- App/lib/walk_view.dart
+++ App/lib/walk_view.dart
@@ -1,15 +1,26 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class WalkView extends StatelessWidget {
-  final String _walkId;
-  WalkView(String walkId) : _walkId = walkId;
+  WalkView(Map<String, dynamic> walk) : _walk = walk;
+
+  final Map<String, dynamic> _walk;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: const Text("Walk View"),
+        title: Text(_walk['name']),
         ),
+      body: Column(
+        children: [
+          Text(_walk['walker']),
+          Text(_walk['date']),
+          for (int i = 0; i < _walk['interested'].length; i++)
+            Text(_walk['interested'][i]),
+        ],
+      ),
     );
   }
 }
