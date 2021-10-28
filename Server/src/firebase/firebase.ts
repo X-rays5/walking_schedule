d9e705bc094764bea6410fc97aecc4be8e56d0ca
X-rays5 <thomas@scheenen.dev>
2021-10-29 00:21:29 +0200

    firebase credential is now a env var

--- Server/src/firebase/firebase.ts
+++ Server/src/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import * as firebase_admin from 'firebase-admin';
-const service_account = require('../../walking-schedule-firebase-adminsdk-gk8zs-f00c265b00.json');
+const service_account = JSON.parse(process.env.FIREBASE_CREDENTIAL);
 export const firebase = firebase_admin.initializeApp({
     credential: firebase_admin.credential.cert(service_account),
 });
