5e585ea25b5dd042da9926bd6534116df8d74eba
X-rays5 <thomas@scheenen.dev>
2021-12-11 23:02:26 +0100

    added a legacy interested array fixer

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import {firebase} from "./firebase/firebase";
+
+// if this is set all legacy data will be updated
+if (process.env.FIXLEGACY === 'true') {
+    console.log('Fixing legacy interested array');
+    firebase.firestore().collection('walks').get().then((docs) => {
+        docs.forEach((doc) => {
+            let interested = doc.data().interested;
+            if (Array.isArray(interested)) {
+                interested = Object.fromEntries(
+                    interested.map((element) => {
+                        if (element === '') {
+                            return ['placeholder', true];
+                        } else {
+                            return [element, true];
+                        }
+                    })
+                );
+                doc.ref.update({
+                    interested: interested
+                }).then(r => {console.log('Fixed legacy interested array');}).catch((err) => {console.error(err);});
+            }
+        })
+    })
+}
+
 import express from 'express';
 const app = express();
 app.use(express.json())
@@ -18,4 +44,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log( `server started at http://localhost:${PORT}` );
-} );
+});
